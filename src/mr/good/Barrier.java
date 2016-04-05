56de8405b91ffb00b8bb0ef349c50a4fd0f80385
LauchlanMcDonald <lauchlan.mcdonald@brentwood.ca>
2016-04-05 10:09:12 -0700

    update

--- src/mr/good/Barrier.java
+++ src/mr/good/Barrier.java
@@ -7,6 +7,7 @@ package mr.good;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Point;
 
 /**
  *
@@ -14,12 +15,81 @@ import java.awt.Graphics;
  */
 class Barrier {
 
-    Barrier(int x, int y, Color colr, GameSurface aThis) {
+//    Barrier(int i, int i0, Color GREEN, GameSurface aThis, CellDataProviderIntf cellData) {
+//    }
 
-    }
 
     public void draw(Graphics graphics) {
-
+        graphics.setColor(Color.BLACK);
+        graphics.fill3DRect(cellData.getSystemCoordX(x, y),
+                cellData.getSystemCoordY(x, y),
+                cellData.getCellWidth(),
+                cellData.getCellHeight(), true);
     }
+
+//<editor-fold defaultstate="collapsed" desc="Constructors">
+    public Barrier(int x, int y, Color color, GameSurface aThis, CellDataProviderIntf cellData) {
+        this.x = x;
+        this.y = y;
+
+        this.color = color;
+        this.cellData = cellData;
+    }
+//</editor-fold>
+
+//<editor-fold defaultstate="collapsed" desc="Properties">
+    private int x, y;
+    private Color color;
+    private CellDataProviderIntf cellData;
+
+    /**
+     * @return the point of the cell location
+     */
+    public Point getLocation() {
+        return new Point(x, y);
+    }
+
+    /**
+     * @return the x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * @param x the x to set
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * @return the y
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * @param y the y to set
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    /**
+     * @return the color
+     */
+    public Color getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(Color color) {
+        this.color = color;
+    }
+//</editor-fold>
 
 }
--- src/mr/good/GameSurface.java
+++ src/mr/good/GameSurface.java
@@ -7,140 +7,44 @@ package mr.good;
 
 import environment.Environment;
 import grid.Grid;
+import images.ResourceTools;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
+import static javax.sound.midi.ShortMessage.START;
 
 /**
  *
  * @author samuelmartinezdelcampo
  */
-public class GameSurface extends Environment {
+public class GameSurface extends Environment implements CellDataProviderIntf, MoveValidatorIntf {
 
     private Grid grid;
     private ArrayList<Barrier> barriers;
+   
+    Image startscreen;
+    private int screen;
 
     public GameSurface() {
 
         grid = new Grid(30, 25, 20, 20, new Point(0, 0), new Color(220, 220, 220));
         
+        startscreen = ResourceTools.loadImageFromResource("Mr.Good/MrGoodStartscreen.jpg");
         
         
         
         
         
-//<editor-fold defaultstate="collapsed" desc="Barriers">
-        barriers = new ArrayList<>();
-        barriers.add(new Barrier(0, 0, Color.GREEN, this));
-        barriers.add(new Barrier(1, 0, Color.GREEN, this));
-        barriers.add(new Barrier(2, 0, Color.GREEN, this));
-        barriers.add(new Barrier(3, 0, Color.GREEN, this));
-        barriers.add(new Barrier(4, 0, Color.GREEN, this));
-        barriers.add(new Barrier(0, 1, Color.GREEN, this));
-        barriers.add(new Barrier(0, 2, Color.GREEN, this));
-        barriers.add(new Barrier(0, 3, Color.GREEN, this));
-        barriers.add(new Barrier(0, 4, Color.GREEN, this));
-        barriers.add(new Barrier(0, 5, Color.GREEN, this));
-        barriers.add(new Barrier(0, 6, Color.GREEN, this));
-        barriers.add(new Barrier(0, 7, Color.GREEN, this));
-        barriers.add(new Barrier(5, 0, Color.GREEN, this));
-        barriers.add(new Barrier(6, 0, Color.GREEN, this));
-        barriers.add(new Barrier(7, 0, Color.GREEN, this));
-        barriers.add(new Barrier(8, 0, Color.GREEN, this));
-        barriers.add(new Barrier(0, 8, Color.GREEN, this));
-        barriers.add(new Barrier(0, 9, Color.GREEN, this));
-        barriers.add(new Barrier(0, 10, Color.GREEN, this));
-        barriers.add(new Barrier(0, 11, Color.GREEN, this));
-        barriers.add(new Barrier(0, 12, Color.GREEN, this));
-        barriers.add(new Barrier(0, 13, Color.GREEN, this));
-        barriers.add(new Barrier(0, 14, Color.GREEN, this));
-        barriers.add(new Barrier(9, 0, Color.GREEN, this));
-        barriers.add(new Barrier(10, 0, Color.GREEN, this));
-        barriers.add(new Barrier(11, 0, Color.GREEN, this));
-        barriers.add(new Barrier(12, 0, Color.GREEN, this));
-        barriers.add(new Barrier(13, 0, Color.GREEN, this));
-        barriers.add(new Barrier(14, 0, Color.GREEN, this));
-        barriers.add(new Barrier(15, 0, Color.GREEN, this));
-        barriers.add(new Barrier(16, 0, Color.GREEN, this));
-        barriers.add(new Barrier(17, 0, Color.GREEN, this));
-        barriers.add(new Barrier(18, 0, Color.GREEN, this));
-        barriers.add(new Barrier(19, 0, Color.GREEN, this));
-        barriers.add(new Barrier(20, 0, Color.GREEN, this));
-        barriers.add(new Barrier(21, 0, Color.GREEN, this));
-        barriers.add(new Barrier(22, 0, Color.GREEN, this));
-        barriers.add(new Barrier(23, 0, Color.GREEN, this));
-        barriers.add(new Barrier(24, 0, Color.GREEN, this));
-        barriers.add(new Barrier(25, 0, Color.GREEN, this));
-        barriers.add(new Barrier(26, 0, Color.GREEN, this));
-        barriers.add(new Barrier(27, 0, Color.GREEN, this));
-        barriers.add(new Barrier(28, 0, Color.GREEN, this));
-        barriers.add(new Barrier(29, 0, Color.GREEN, this));
-        barriers.add(new Barrier(0, 15, Color.GREEN, this));
-        barriers.add(new Barrier(0, 16, Color.GREEN, this));
-        barriers.add(new Barrier(0, 17, Color.GREEN, this));
-        barriers.add(new Barrier(0, 18, Color.GREEN, this));
-        barriers.add(new Barrier(0, 19, Color.GREEN, this));
-        barriers.add(new Barrier(0, 20, Color.GREEN, this));
-        barriers.add(new Barrier(0, 21, Color.GREEN, this));
-        barriers.add(new Barrier(0, 22, Color.GREEN, this));
-        barriers.add(new Barrier(0, 23, Color.GREEN, this));
-        barriers.add(new Barrier(0, 24, Color.GREEN, this));
-        barriers.add(new Barrier(1, 24, Color.GREEN, this));
-        barriers.add(new Barrier(2, 24, Color.GREEN, this));
-        barriers.add(new Barrier(3, 24, Color.GREEN, this));
-        barriers.add(new Barrier(4, 24, Color.GREEN, this));
-        barriers.add(new Barrier(5, 24, Color.GREEN, this));
-        barriers.add(new Barrier(6, 24, Color.GREEN, this));
-        barriers.add(new Barrier(7, 24, Color.GREEN, this));
-        barriers.add(new Barrier(8, 24, Color.GREEN, this));
-        barriers.add(new Barrier(9, 24, Color.GREEN, this));
-        barriers.add(new Barrier(10, 24, Color.GREEN, this));
-        barriers.add(new Barrier(11, 24, Color.GREEN, this));
-        barriers.add(new Barrier(12, 24, Color.GREEN, this));
-        barriers.add(new Barrier(13, 24, Color.GREEN, this));
-        barriers.add(new Barrier(14, 24, Color.GREEN, this));
-        barriers.add(new Barrier(15, 24, Color.GREEN, this));
-        barriers.add(new Barrier(16, 24, Color.GREEN, this));
-        barriers.add(new Barrier(17, 24, Color.GREEN, this));
-        barriers.add(new Barrier(18, 24, Color.GREEN, this));
-        barriers.add(new Barrier(19, 24, Color.GREEN, this));
-        barriers.add(new Barrier(20, 24, Color.GREEN, this));
-        barriers.add(new Barrier(21, 24, Color.GREEN, this));
-        barriers.add(new Barrier(22, 24, Color.GREEN, this));
-        barriers.add(new Barrier(23, 24, Color.GREEN, this));
-        barriers.add(new Barrier(24, 24, Color.GREEN, this));
-        barriers.add(new Barrier(25, 24, Color.GREEN, this));
-        barriers.add(new Barrier(26, 24, Color.GREEN, this));
-        barriers.add(new Barrier(27, 24, Color.GREEN, this));
-        barriers.add(new Barrier(28, 24, Color.GREEN, this));
-        barriers.add(new Barrier(29, 24, Color.GREEN, this));
-        barriers.add(new Barrier(29, 1, Color.GREEN, this));
-        barriers.add(new Barrier(29, 2, Color.GREEN, this));
-        barriers.add(new Barrier(29, 3, Color.GREEN, this));
-        barriers.add(new Barrier(29, 4, Color.GREEN, this));
-        barriers.add(new Barrier(29, 5, Color.GREEN, this));
-        barriers.add(new Barrier(29, 6, Color.GREEN, this));
-        barriers.add(new Barrier(29, 7, Color.GREEN, this));
-        barriers.add(new Barrier(29, 8, Color.GREEN, this));
-        barriers.add(new Barrier(29, 9, Color.GREEN, this));
-        barriers.add(new Barrier(29, 10, Color.GREEN, this));
-        barriers.add(new Barrier(29, 11, Color.GREEN, this));
-        barriers.add(new Barrier(29, 12, Color.GREEN, this));
-        barriers.add(new Barrier(29, 13, Color.GREEN, this));
-        barriers.add(new Barrier(29, 14, Color.GREEN, this));
-        barriers.add(new Barrier(29, 15, Color.GREEN, this));
-        barriers.add(new Barrier(29, 16, Color.GREEN, this));
-        barriers.add(new Barrier(29, 17, Color.GREEN, this));
-        barriers.add(new Barrier(29, 18, Color.GREEN, this));
-        barriers.add(new Barrier(29, 19, Color.GREEN, this));
-        barriers.add(new Barrier(29, 20, Color.GREEN, this));
-        barriers.add(new Barrier(29, 21, Color.GREEN, this));
-        barriers.add(new Barrier(29, 22, Color.GREEN, this));
-        barriers.add(new Barrier(29, 23, Color.GREEN, this));
-        barriers.add(new Barrier(29, 24, Color.GREEN, this));
+        
+//<editor-fold defaultstate="collapsed" desc="Barriers">//<editor-fold defaultstate="collapsed" desc="Barriers">
+        
+
+//</editor-fold>
     }
 
     @Override
@@ -153,6 +57,7 @@ public class GameSurface extends Environment {
 
     @Override
     public void keyPressedHandler(KeyEvent e) {
+        
     }
 
     @Override
@@ -166,15 +71,74 @@ public class GameSurface extends Environment {
     @Override
     public void paintEnvironment(Graphics graphics) {
 
-        if (grid != null) {
-            grid.paintComponent(graphics);
+        if (getGrid() != null) {
+            getGrid().paintComponent(graphics);
         }
 
-        if (barriers != null) {
-            for (Barrier barrier : barriers) {
+        if (getBarriers() != null) {
+            for (Barrier barrier : getBarriers()) {
                 barrier.draw(graphics);
             }
         }
+        
+    
+        switch (screen) {
+            case START:
+                graphics.setFont(new Font("ARIAL", Font.BOLD, 40));
+                graphics.drawImage(startscreen, 0, 0, 900, 580, this);
+                graphics.drawString("PRESS SPACE TO START", 215, 550);
+
+                break;
     }
+
+    /**
+     * @return the grid
+     */
+    public Grid getGrid() {
+        return grid;
+    }
+
+    /**
+     * @param grid the grid to set
+     */
+    public void setGrid(Grid grid) {
+        this.grid = grid;
+    }
+
+    /**
+     * @return the barriers
+     */
+    public ArrayList<Barrier> getBarriers() {
+        return barriers;
+    }
+
+    /**
+     * @param barriers the barriers to set
+     */
+    public void setBarriers(ArrayList<Barrier> barriers) {
+        this.barriers = barriers;
+    }
+
+//<editor-fold defaultstate="collapsed" desc="CellDataProviderIntf">
+    @Override
+    public int getCellWidth() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public int getCellHeight() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public int getSystemCoordX(int x, int y) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public int getSystemCoordY(int x, int y) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+//</editor-fold>
 
 }
